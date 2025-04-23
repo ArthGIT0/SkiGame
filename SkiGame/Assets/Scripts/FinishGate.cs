786fc556332dfc653472843bb7ac6fc9abee523c
ArthGIT0 <arturumbrashko@gmail.com>
2025-04-23 13:34:40 +0300

    Revert "a"
    This reverts commit 05f292009d71af0007182c77717580231a2a7328.

--- SkiGame/Assets/Scripts/FinishGate.cs
+++ SkiGame/Assets/Scripts/FinishGate.cs
@@ -1,13 +1,8 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FinishGate : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-            GameEvents.CallRaceFinish();
-    }
+    
 }
